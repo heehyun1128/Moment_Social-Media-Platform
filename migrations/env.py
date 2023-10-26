b37eff7baf2346bd1f2417e3ad6587fbc62b0c2a
heehyun1128 <chenyiapp@yahoo.com>
2023-10-25 20:56:38 -0700

    migration

--- migrations/env.py
+++ migrations/env.py
@@ -11,6 +11,8 @@ from alembic import context
 import os
 environment = os.getenv("FLASK_ENV")
 SCHEMA = os.environ.get("SCHEMA")
+
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
@@ -38,15 +40,12 @@ target_metadata = current_app.extensions['migrate'].db.metadata
 
 def run_migrations_offline():
     """Run migrations in 'offline' mode.
-
     This configures the context with just a URL
     and not an Engine, though an Engine is acceptable
     here as well.  By skipping the Engine creation
     we don't even need a DBAPI to be available.
-
     Calls to context.execute() here emit the given string to the
     script output.
-
     """
     url = config.get_main_option("sqlalchemy.url")
     context.configure(
@@ -59,10 +58,8 @@ def run_migrations_offline():
 
 def run_migrations_online():
     """Run migrations in 'online' mode.
-
     In this scenario we need to create an Engine
     and associate a connection with the context.
-
     """
 
     # this callback is used to prevent an auto-migration from being generated
@@ -97,7 +94,6 @@ def run_migrations_online():
             if environment == "production":
                 context.execute(f"SET search_path TO {SCHEMA}")
             context.run_migrations()
-
 
 if context.is_offline_mode():
     run_migrations_offline()
